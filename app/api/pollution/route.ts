f1620cc10ea0ae006729a18d8cdd9df967cb8b12
Amrit <amritdubey22003@gmail.com>
2025-08-10 09:51:27 +0530

    Redis Added

--- app/api/pollution/route.ts
+++ app/api/pollution/route.ts
@@ -1,4 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "redis";
+
+const redis = createClient({ url: process.env.REDIS_URL });
+
+redis.on('error', (err) => console.log(err));
+
+async function startRedis() {
+    if(!redis.isOpen) await redis.connect();
+}
+
+startRedis();
 
 export async function GET(req: NextRequest){
     const { searchParams } = new URL(req.url);
@@ -8,15 +19,25 @@ export async function GET(req: NextRequest){
 
     if(!lat || !lon) return NextResponse.json({error: 'Lat/Lon missing'}, {status: 400});
 
+    const cachedKey = `lat=${lat}lon=${lon}`;
+
     try {
+        const canchedVal = await redis.get(cachedKey);
+
+        if(canchedVal) return NextResponse.json(JSON.parse(canchedVal), {status: 200});
+
         const response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`)
         if(!response.ok) return NextResponse.json({error: 'Couldnt fetch pollution data'}, {status: 400});
 
         const json = await response.json();
-        return NextResponse.json({
+        const data = {
             aqi: json.list[0].main.aqi,
             pm2_5: json.list[0].components.pm2_5
-        }, {status: 200});
+        };
+
+        await redis.setEx(cachedKey, 600, JSON.stringify(data));
+
+        return NextResponse.json(data, {status: 200});
     } catch (error) {
         return NextResponse.json({error: 'Something went wrong', details: String(error)}, {status: 400});
     }
